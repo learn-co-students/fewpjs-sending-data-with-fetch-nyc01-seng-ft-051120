64f771dadd5a8cc6cc36755ec796044d4074cf0a
https://learn.co/christa-gammage <crgammage@gmail.com>
2020-06-26 08:51:22 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+function submitData(name, email) {
+    let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type" : "application/json",
+            "Accept": "application/json"
+        }, body: JSON.stringify({
+            name,
+            email
+        })
+    }
+    return fetch("http://localhost:3000/users", configObj)
+    .then(response => response.json())
+    .then(data => {
+        document.body.innerHTML = data["id"]
+    })
+    .catch(error => {
+        document.body.innerHTML = error.message
+    })
+}
+
+submitData()
